70c60e258ad73120b67c890c32db8535b12ec0a1
MBDesu <jordyn.tucker@protonmail.com>
2022-07-23 02:17:30 -0400

    Day 3 part 1

---
+++ Day 3/part-1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <math.h>
+
+int calc_epsilon(int gamma) {
+  // lmao it works on 64 bit machines, i promise
+  return ~gamma ^ 0b1111111111111111111111111111111111111111111111111111000000000000;
+}
+
+int calc_gamma(int* counts, int num_lines) {
+  int gamma = 0;
+  for (size_t i = 0; i < 12; i++) {
+    int most_common = counts[i] > (num_lines / 2) ? 1 : 0;
+    if (most_common) {
+      gamma += pow(2, 11 - i);
+    }
+  }
+  return gamma;
+}
+
+void parse_line(std::ifstream *file, int *counts) {
+  char line[256];
+  file->getline(line, sizeof(line));
+  for (size_t i = 0; i < 12; i++) {
+    counts[i] += line[i] - '0';
+  }
+}
+
+int parse_file(std::ifstream *file) {
+  int result = 0;
+  int num_lines = 0;
+  int counts[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  while (!file->eof()) {
+    ++num_lines;
+    parse_line(file, counts);
+  }
+  int gamma = calc_gamma(counts, num_lines);
+  return gamma * calc_epsilon(gamma);
+}
+
+int main() {
+  std::ifstream file;
+  file.open("input.txt", std::ifstream::in);
+  std::cout << parse_file(&file) << std::endl;
+  return 0;
+}
